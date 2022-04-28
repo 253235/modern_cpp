b9656a8d28ee34b987bdcbd6012c74773c298b05
Jędrzej Urbaniak <253235@student.pwr.edu.pl>
2022-04-28 16:51:17 +0200

    marked Fibonacci() as constexpr

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -1,9 +1,7 @@
-#include "Fibonacci.hpp"
-
-int Fibonacci(int n)
+constexpr int Fibonacci(int n)
 {
     if(n <= 1) return n;
-    int n1, n2, result;
+    int n1 = 0, n2 = 0, result = 0;
     n2 = 0;
     n1 = 1;
     for(int i = 0; i < n-1; i++)
